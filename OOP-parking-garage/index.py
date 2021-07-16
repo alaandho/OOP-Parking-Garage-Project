02f488da746cfa1460b4edf7e3c0fc85e7075ca6
Ala Ho <alaandho@gmail.com>
2021-07-16 13:05:21 -0400

    OOP

---
+++ OOP-parking-garage/index.py
@@ -0,0 +1,57 @@
+#Attributes:
+    # tickets -> list
+    # parkingSpaces -> list
+    # currentTicket -> dictionary
+
+
+
+
+
+
+
+
+
+
+#takeTicket
+    # descrease tickets -1
+    # decrease parkingSpaces -1
+
+
+
+
+
+
+
+
+
+
+
+
+#payForParking
+    # display input('please wait for amount')
+        #store in variable
+    
+    #If statement
+        #if user has balance then pay
+
+
+
+        
+        #if user has no balance, then ticket is paid
+            #print ('you have 15 minutes to leave')
+    # update currentTicket dictionary key 'paid' = True
+
+
+
+
+
+
+
+
+
+
+
+
+
+#leaveGarage
+    # If ticket paid -> print ('Thank you, have a nice day')
